543ded8bcb183d46e0cb0c5b5dc35a0a15b4d724
haileyng02 <haileynguyenss@gmail.com>
2023-03-24 01:07:44 +0700

    added create new post modal

--- src/components/home/Post.jsx
+++ src/components/home/Post.jsx
@@ -67,7 +67,7 @@ const Post = () => {
         <Divider className="border-black15 mt-[10px] mb-[6px]" />
         <div className="row gap-x-[13px]">
           <div className="w-[30px] h-[30px] bg-grey rounded-full"></div>
-          <input className="w-full bg-transparent hover:border-none focus:outline-none flex-1" />
+          <input placeholder="Add a comment..." className="w-full bg-transparent hover:border-none focus:outline-none flex-1 text-14 placeholder:text-black50" />
           <button className="font-bold text-14 text-[#96CAF7]">Post</button>
         </div>
       </div>
--- src/components/modals/CreatePostModal.jsx
+++ src/components/modals/CreatePostModal.jsx
@@ -1,13 +1,38 @@
+import { Upload } from "antd";
 import Modal from "./Modal";
+import imageIcon from "../../assets/image.svg";
+
+const { Dragger } = Upload;
 
 const CreatePostModal = ({ open, onCancel }) => {
   return (
-    <Modal
-      open={open}
-      onCancel={onCancel}
-      title="Create new post"
-    >
-      <div className="px-[20px] py-[14px]"></div>
+    <Modal open={open} onCancel={onCancel} title="Create new post">
+      <div className="px-[20px] py-[14px]">
+        {/* Upload */}
+        <Dragger showUploadList={false}>
+          <div className="flex flex-col items-center">
+            <div className="w-[100px] h-[100px] bg-[#77777740] rounded-full center">
+              <img src={imageIcon} alt="Upload" />
+            </div>
+            <h1 className="font-semibold text-[20px] leading-[24px] text-[#676767] mt-[7px]">
+              Add photos/videos
+            </h1>
+            <p className="text-[12px] leading-[15px] text-[#676767]">
+              Or drag and drop
+            </p>
+          </div>
+        </Dragger>
+        {/* User info */}
+        <div className="row gap-x-[7px] mt-[13px]">
+          <div className="w-[30px] avatar"></div>
+          <h3 className="font-semibold text-14">_ptt.chang</h3>
+        </div>
+        {/* Caption */}
+        <textarea
+          placeholder="Write a caption..."
+          className="resize-none w-full mt-[13px] bg-transparent focus:outline-none text-14 placeholder:text-black50 h-[100px]"
+        />
+      </div>
     </Modal>
   );
 };
