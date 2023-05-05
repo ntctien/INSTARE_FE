1769a8027933cba1057d5da4c60c670171aa6bab
haileyng02 <haileynguyenss@gmail.com>
2023-05-06 01:44:57 +0700

    update story layout

--- src/layouts/CreateStoryLayout/index.jsx
+++ src/layouts/CreateStoryLayout/index.jsx
@@ -6,7 +6,8 @@ const CreateStoryLayout = () => {
   return (
     <div className='flex h-screen'>
         <div className='bg-[#96CAF726] w-[340px] rounded-r-15'></div>
-        <div className='flex-1'>
+        <div className='flex-1 flex flex-col items-center'>
+            <h1 className='font-ubuntu font-bold text-32 mt-5'>Create your story</h1>
             <Outlet/>
         </div>
         <div className='bg-[#BFB2F326] w-[300px] rounded-l-10'>
--- src/pages/CreateStory.jsx
+++ src/pages/CreateStory.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const CreateStory = () => {
   return (
-    <div>CreateStory</div>
+    <div>
+    </div>
   )
 }
 
