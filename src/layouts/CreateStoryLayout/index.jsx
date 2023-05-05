d0c96375000456f12f1f736ed752a7b067215a79
haileyng02 <haileynguyenss@gmail.com>
2023-05-06 01:40:07 +0700

    update create story layout

--- src/layouts/CreateStoryLayout/index.jsx
+++ src/layouts/CreateStoryLayout/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 import UserInfo from '~/components/home/user_info/UserInfo'
 
 const CreateStoryLayout = () => {
   return (
     <div className='flex h-screen'>
         <div className='bg-[#96CAF726] w-[340px] rounded-r-15'></div>
-        <div className='flex-1'></div>
+        <div className='flex-1'>
+            <Outlet/>
+        </div>
         <div className='bg-[#BFB2F326] w-[300px] rounded-l-10'>
             <UserInfo/>
         </div>
