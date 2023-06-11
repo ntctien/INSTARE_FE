1fdb3cee6a80c6f3b57b4d1f9ab10f342f7ae632
haileyng02 <haileynguyenss@gmail.com>
2023-06-11 17:29:45 +0700

    update font Ubuntu việt hóa

---
+++ src/fonts/Ubuntu/index.js
@@ -0,0 +1,4 @@
+import '~/fonts/Ubuntu/Ubuntu-L.ttf'
+import '~/fonts/Ubuntu/Ubuntu-R.ttf';
+import '~/fonts/Ubuntu/Ubuntu-M.ttf';
+import '~/fonts/Ubuntu/Ubuntu-B.ttf';
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import thunk from 'redux-thunk'
 import './index.css';
 import App from './App';
 import allReducers from './reducers';
+import '~/fonts/Ubuntu';
 import reportWebVitals from './reportWebVitals';
 
 const middleware = [thunk]
